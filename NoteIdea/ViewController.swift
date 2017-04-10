add0256ca6b248fd37a2ae64924ef2b88efbf700
Thalles Henrique <thalleshenrique@Thalless-Mac.local>
2017-04-10 15:04:09 -0300

    Initial Commit

---
+++ NoteIdea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteIdea
+//
+//  Created by Thalles Henrique on 10/04/17.
+//  Copyright © 2017 Thalles Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
